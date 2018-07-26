fe19120ec44817c75714bb500b57673e725009c1
Cote <majovardon@gmail.com>
2018-07-25 20:31:48 -0400

    Geolocalización funcional

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,37 @@
-var map;
+// Aquí se muestra el mapa 
+var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.45694, lng: -70.64827},
+    center: { lat: -34.397, lng: 150.644 },
     zoom: 15
   });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Aquí se nos pide la localización para buscarla en el mapa
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Caso errores
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
